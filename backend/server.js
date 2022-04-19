ec04e3ed4bb96449c539e3aec7dc42b55ddd45eb
Michael Grumbine <mgrumbine3@gmail.com>
2022-04-19 12:07:58 -0700

    home path

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,9 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+app.get('/', (req, res) => {
+  res.send('You made it');
+});
 app.get('/welcome', (req, res) => res.send('Fish and Chips!'));
 app.use('/test', routes);
 
