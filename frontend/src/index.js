cf28aa4074c15be63b740c5dea6498cacaa44b6d
Michael Grumbine <mgrumbine3@gmail.com>
2022-04-21 20:31:10 -0700

    also including database creation/setup

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,17 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import thunk from 'redux-thunk';
+import { applyMiddleware, legacy_createStore as createStore } from 'redux';
+import rootReducer from './reducers/rootreducer';
 import App from './App';
 import Login from './Login';
 import Signup from './Signup';
-
-const createStore = 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <Routes store={}>
+      <Routes store={createStore(rootReducer, applyMiddleware(thunk))}>
         <Route path="login" element={<Login />} />
         <Route path="signup" element={<Signup />} />
         <Route path="*" element={<App />} />
